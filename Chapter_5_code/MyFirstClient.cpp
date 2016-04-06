4e344f34971d2d0b6aca26f252c4b105be2242e9
charles-pku-2013 <chao.sun.2010@gmail.com>
2016-04-06 19:15:49 +0800

    Test multiplex, use ThreadpoolServer

--- Chapter_5_code/MyFirstClient.cpp
+++ Chapter_5_code/MyFirstClient.cpp
@@ -2,33 +2,87 @@
 
 // MyFirstService header file
 #include "gen-cpp/MyFirstService.h"
-
+#include <ctime>
+#include <functional>
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/protocol/TMultiplexedProtocol.h>
+#include <boost/thread.hpp>
+#include <boost/chrono/chrono.hpp>
+#include <glog/logging.h>
 
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
-
 using namespace std;
-
 using namespace myfirst;
 
-int main(int argc, char **argv) {
-    boost::shared_ptr<TSocket> socket(new TSocket("localhost", 8080));
-    boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-    boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+/*
+ * client上虽然有多个线程同时发出请求，但都是走的同一个连接，因此server上用一个线程服务这些请求。
+ * I0406 17:10:00.693013  4647 MyFirstServer.cpp:35] Server thread 7fe2057ba700 doing multiply 9 * 3
+ * I0406 17:10:05.694567  4647 MyFirstServer.cpp:35] Server thread 7fe2057ba700 doing multiply 17 * 10
+ * I0406 17:10:10.696091  4647 MyFirstServer.cpp:35] Server thread 7fe2057ba700 doing multiply 4 * 18
+ */
+static
+void test1( MyFirstServiceClient &client )
+{
+    int n1 = rand() % 30;
+    int n2 = rand() % 30;
 
-    MyFirstServiceClient client(protocol);
+    LOG(INFO) << "Thread " << boost::this_thread::get_id() << " making request: " << n1 << " * " << n2;
+    int result = client.multiply( n1, n2 );
+    LOG(INFO) << "Thread " << boost::this_thread::get_id() << " got answer: " << result;
+}
 
-    transport->open();
+static
+void test2( const boost::shared_ptr<TProtocol> &_protocol )
+{
+    using namespace std;
 
-    client.log("logfile.log");
+    int n1 = rand() % 30;
+    int n2 = rand() % 30;
 
-    int result = client.multiply(6,7);
-    cout << result << endl;
-    transport->close();
+    try {
+        // 服务器端要用 TMultiplexedProcessor
+        // 但还是用一个线程处理同一连接的不同请求。
+        boost::shared_ptr<TProtocol> protocol(new TMultiplexedProtocol(_protocol, "Service1"));
+        MyFirstServiceClient client(protocol);
+        LOG(INFO) << "Thread " << boost::this_thread::get_id() << " making request: " << n1 << " * " << n2;
+        int result = client.multiply( n1, n2 );
+        LOG(INFO) << "Thread " << boost::this_thread::get_id() << " got answer: " << result;
+
+    } catch ( const exception &ex ) {
+        cout << "Thread " << boost::this_thread::get_id() << " caught exception: " << ex.what() << endl;
+    } // try
+}
+
+int main(int argc, char **argv) 
+{
+    try {
+        google::InitGoogleLogging(argv[0]);
+
+        boost::shared_ptr<TSocket> socket(new TSocket("localhost", 8080));
+        boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+        boost::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+
+        MyFirstServiceClient client(protocol);
+
+        transport->open();
+
+        srand( time(0) );
+        boost::thread_group thrgroup;
+        for( uint32_t i = 0; i < 1; ++i )
+            thrgroup.create_thread( std::bind(test2, protocol) );
+            // thrgroup.create_thread( std::bind(test1, std::ref(client)) );
+        thrgroup.join_all();
+        cout << "Test finished!" << endl;
+
+        transport->close();
+
+    } catch ( const exception &ex ) {
+        cout << "Exception: " << ex.what() << endl;
+    } // try
 
     return 0;
 }
--- Chapter_5_code/MyFirstServer.cpp
+++ Chapter_5_code/MyFirstServer.cpp
@@ -1,11 +1,18 @@
 #include "MyFirstService.h"
+#include <thrift/concurrency/ThreadManager.h>
+#include <thrift/concurrency/PlatformThreadFactory.h>
 #include <thrift/protocol/TBinaryProtocol.h>
-// #include <thrift/server/TSimpleServer.h>
 #include <thrift/server/TThreadPoolServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <thrift/processor/TMultiplexedProcessor.h>
+#include <boost/make_shared.hpp>
+#include <boost/thread.hpp>
+#include <boost/chrono/chrono.hpp>
+#include <glog/logging.h>
 
 using namespace ::apache::thrift;
+using namespace ::apache::thrift::concurrency;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
@@ -15,37 +22,69 @@ using boost::shared_ptr;
 using namespace  ::myfirst;
 
 class MyFirstServiceHandler : virtual public MyFirstServiceIf {
- public:
-  MyFirstServiceHandler() {
-    // Your initialization goes here (if needed)
-  }
+public:
+    MyFirstServiceHandler() {
+        // Your initialization goes here (if needed)
+    }
 
-  void log(const std::string& filename) {
-    // implementation of log function goes here
-  }
+    void log(const std::string& filename) {
+        // implementation of log function goes here
+    }
 
-  int multiply(const int number1, const int number2) {
-    // implementation of multiply function goes here
-    return (number1 * number2);
-  }
+    int multiply(const int number1, const int number2) 
+    {
+        LOG(INFO) << "Server thread " << boost::this_thread::get_id() 
+                << " doing multiply " << number1 << " * " << number2;
+        boost::this_thread::sleep_for(boost::chrono::seconds(5));
+        return (number1 * number2);
+    }
 
-  int get_log_size(const std::string& filename) {
-    // implementation of get_log_size function goes here
-    return 0;
-  }
+    int get_log_size(const std::string& filename) {
+        // implementation of get_log_size function goes here
+        return 0;
+    }
 
 };
 
-int main(int argc, char **argv) {
-  int port = 8080;
-  shared_ptr<MyFirstServiceHandler> handler(new MyFirstServiceHandler());
-  shared_ptr<TProcessor> processor(new MyFirstServiceProcessor(handler));
-  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+int main(int argc, char **argv) 
+{
+    const int port = 8080;
 
-  TThreadPoolServer server(processor, serverTransport, transportFactory, protocolFactory);
-  server.serve();
-  return 0;
+    using std::cout; using std::endl; using std::exception;
+
+    try {
+        google::InitGoogleLogging(argv[0]);
+
+        shared_ptr<MyFirstServiceHandler> handler(new MyFirstServiceHandler());
+        shared_ptr<TProcessor> processor(new MyFirstServiceProcessor(handler));
+        shared_ptr<TMultiplexedProcessor> mprocessor(new TMultiplexedProcessor);
+        mprocessor->registerProcessor("Service1", processor);
+
+        shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+        shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+        shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+
+        // TODO n_cpu
+        const int workerCount = 8;
+
+        // tutorial/cpp/CppServer.cpp
+        boost::shared_ptr<ThreadManager> threadManager =
+            ThreadManager::newSimpleThreadManager(workerCount);
+        threadManager->threadFactory(
+                boost::make_shared<PlatformThreadFactory>());
+        threadManager->start();
+
+        cout << "Starting server..." << endl;
+        // This server allows "workerCount" connection at a time, and reuses threads
+        TThreadPoolServer server(mprocessor, serverTransport, 
+                transportFactory, protocolFactory, threadManager);
+        server.serve();
+        cout << "Done!" << endl;
+
+    } catch ( const exception &ex ) {
+        cout << "Exception: " << ex.what() << endl;
+    } // try
+
+    return 0;
 }
 
